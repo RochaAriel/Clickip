2f3cca7f05f85add9065c170d250bb6e0af391f2
Ariel Rocha <arielalves.alves@gmail.com>
2023-07-17 21:26:24 -0300

    Card Editor

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 let subMenu = document.getElementById("subMenu");
-        function clickmenu(){
-            subMenu.classList.toggle("open-menu");
-        }
+function clickmenu() {
+  subMenu.classList.toggle("open-menu");
+}
 
 
-        document.querySelectorAll(".card").forEach(function(card) {
-            card.addEventListener("mouseenter", function() {
-              this.classList.add("hover");
-            });
-          
-            card.addEventListener("mouseleave", function() {
-              this.classList.remove("hover");
-            });
-          });       
+document.querySelectorAll(".card").forEach(function (card) {
+  card.addEventListener("mouseenter", function () {
+    this.classList.add("hover");
+  });
+
+  card.addEventListener("mouseleave", function () {
+    this.classList.remove("hover");
+  });
+});       
